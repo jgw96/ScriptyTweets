f20f1ea95a3c2e9c53cbe5a3544e3dc9181da48b
Justin Willis <jgw9617@gmail.com>
2016-01-02 15:02:15 -0600

    compiled with babel

---
+++ public/compiled.js
@@ -0,0 +1,51 @@
+/* global io */
+
+'use strict';
+
+var socket = io();
+var tweets = [];
+
+var list = document.querySelector("#tweetList");
+
+var urlify = function urlify(text) {
+    var urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, function (url) {
+        return '<a href="' + url + '">' + url + '</a>';
+    });
+};
+
+document.querySelector("#startTweets").addEventListener("click", function () {
+    //start getting tweets
+    socket.emit("im ready");
+
+    //show toast
+    document.querySelector("#toast0").open();
+});
+
+socket.on("tweet", function (tweet) {
+    tweets.push(tweet);
+
+    for (var i in tweets) {
+        var urlTweet = urlify(tweets[i].text);
+        var para = document.createElement("p");
+        para.innerHTML = urlTweet;
+
+        var image = document.createElement("img");
+        image.src = tweets[i].user.profile_image_url;
+        image.style.width = "5em";
+        image.style.borderRadius = "50%";
+        image.style.marginRight = "1em";
+
+        var listItem = document.createElement("paper-item");
+        listItem.style.width = "80%";
+        listItem.style.marginBottom = "2%";
+        listItem.appendChild(image);
+        listItem.appendChild(para);
+
+        list.appendChild(listItem);
+    }
+
+    if (tweets.length > 30) {
+        tweets.length = 0;
+    }
+});
